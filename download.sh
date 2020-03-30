c68d53c84c974acce61cd5a6f316c0719618cc16
Jason Song <i@wolfogre.com>
2020-03-30 18:16:15 +0800

    feat: download script

---
+++ download.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+KEY=$(cat key.txt)
+
+wget --content-disposition "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${KEY}&suffix=tar.gz"
+wget --content-disposition "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-City&license_key=${KEY}&suffix=tar.gz.sha256"
+
